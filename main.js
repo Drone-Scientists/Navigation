767a0e88a67da1a63a77cbe5d57c42e7fab7f86c
Everett <Cooleyeverett4@gmail.com>
2022-02-19 23:11:38 -0800

    Add random rectangles

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-let square_top = 50;
-let square_left = 50;
+const cWidth = 1200
+const cHeight = 800 
 
-let body = document.getElementById("main-body");
-body.addEventListener("keydown", function (e) {
-    if (e.key === "w") {
-        square_top -= 1;
-        document.getElementById("s1").style.top = square_top + "%";
-    }
-    else if(e.key === "s"){
-        square_top += 1;
-        document.getElementById("s1").style.top = square_top + "%"; 
-    }
-    else if(e.key === "a"){
-        square_left -= 1;
-        document.getElementById("s1").style.left = square_left + "%"; 
-    }
-    else if(e.key === "d"){
-        square_left += 1;
-        document.getElementById("s1").style.left = square_left + "%"; 
-    }
-    if(e.key === "d" && e.key === "w"){
-        square_left += 1;
-        square_top -= 1;
-        document.getElementById("s1").style.left = square_left + "%"; 
-        document.getElementById("s1").style.left = square_top + "%"; 
-    }
-});
+// generates a random number between a min and max
+function randomPosition(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// draw rectangle to canvas
+function addRectangle(){
+    var canvas = document.getElementById("canvas1");
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#D51419";
+    l1 = randomPosition(10, cWidth);
+    l2 = randomPosition(10, cHeight);
+    l3 = randomPosition(20, 200);
+    l4 = randomPosition(20, 200);
+    // console.log(l1)
+    context.fillRect(l1, l2, l3, l4);
+}
+
